82a511b1c424eac7c291b0b9f39e52d00e178072
Ignacio <ignacampos@hotmail.com>
2025-01-07 19:24:17 -0300

    navbar responsive

--- viajes-app/src/components/Navbar.tsx
+++ viajes-app/src/components/Navbar.tsx
@@ -1,9 +1,15 @@
 // src/components/Navbar.tsx
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar: FC = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       {/* Logo */}
@@ -12,15 +18,20 @@ const Navbar: FC = () => {
         <div className="logo-dot" />
       </div>
 
+      {/* Hamburger Menu Icon */}
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        &#9776; {/* This is the hamburger icon */}
+      </div>
+
       {/* Navigation Links */}
-      <div className="nav-links">
+      <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
         <Link to="/" className="nav-link">Inicio</Link>
         <Link to="/como-funciona" className="nav-link">Como funciona</Link>
         <Link to="/beneficios" className="nav-link">Beneficios</Link>
       </div>
 
       {/* Auth Links */}
-      <div className="auth-buttons">
+      <div className={`auth-buttons ${isMenuOpen ? 'open' : ''}`}>
         <Link to="/login" className="nav-link">
           Iniciar Sesión
         </Link>
