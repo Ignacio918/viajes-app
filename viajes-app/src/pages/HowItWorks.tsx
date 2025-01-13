1a859d96949c9437191269fd2a95f105140fa8af
Ignacio <ignacampos@hotmail.com>
2025-01-13 20:22:23 -0300

    continuando con how it works section

--- viajes-app/src/pages/HowItWorks.tsx
+++ viajes-app/src/pages/HowItWorks.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 
 const HowItWorks: React.FC = () => {
   return (
-    <section className="how-it-works">
-      {/* Aquí irá el contenido del diseño de Figma */}
+    <section className="relative w-full h-full bg-[#FDFDFD]">
+      <div className="absolute w-full h-full">
+        <div className="absolute w-[1440px] h-[810px] left-0 top-0 bg-gradient-radial from-transparent to-black"></div>
+        <img 
+          className="absolute w-[537px] h-[768px] left-[124px] top-[21px] shadow-lg rounded-2xl" 
+          src="https://via.placeholder.com/537x768" 
+          alt="Random"
+        />
+      </div>
     </section>
   );
 };
