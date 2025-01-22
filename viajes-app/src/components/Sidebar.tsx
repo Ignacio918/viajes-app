733ceb748c7d0abc0cf59db590ae4ef6a2a22dc6
Ignacio <ignacampos@hotmail.com>
2025-01-21 23:31:50 -0300

    empezamos el dashbaord

---
+++ viajes-app/src/components/Sidebar.tsx
@@ -0,0 +1,77 @@
+// src/components/Sidebar.tsx
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Sidebar.css';
+
+const Sidebar: React.FC = () => {
+    return (
+        <div className="sidebar">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink 
+                            to="/dashboard" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Inicio
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/dashboard/actividades" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Actividades
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/dashboard/traslados" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Traslados
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/dashboard/vuelos" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Vuelos
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/dashboard/hoteles" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Hoteles
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink 
+                            to="/dashboard/chat-ai" 
+                            className={({ isActive }) => 
+                                isActive ? "active" : "inactive"
+                            }
+                        >
+                            Chat AI
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Sidebar;
